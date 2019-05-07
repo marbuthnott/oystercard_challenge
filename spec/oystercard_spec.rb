c851accf40969cd0b724d72c4c7127828dfc46c7
Student <magnusarbuthnott@gmail.com>
2019-05-07 16:53:25 +0100

    Test oystercard has balance of zero. Added balance variable and attr_reader. Passed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,14 @@
+require 'oystercard'
+
+# in spec/oystercard_spec.rb
 describe Oystercard do
-  
+
+  it 'has a balance of zero' do
+    expect(subject.balance).to eq(0)
+  end
+
 end
+
+# ISSUE COMMENTS - NameError: uninitialized constant Oystercard
+# File path = ./spec/oystercard_spec
+# LINE OF ERROR = # ./spec/oystercard_spec.rb:2:in `<top (required)>'
